a9b18aa6325cda85eada229a1b0fceff514c9401
Saifdev22 <127614161+Saifdev22@users.noreply.github.com>
2024-05-19 12:29:14 +0200

    Update Program.cs

--- PresentationLayer/Server/Program.cs
+++ PresentationLayer/Server/Program.cs
@@ -76,11 +76,13 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseExceptionHandler(_ => { });
 
